db62cc7f055dd71e129dce2db7dc69359efa5832
Chuck J Hardy <chuckjhardy@gmail.com>
2019-01-05 00:05:20 +0000

    Improve Install Script Formattingwq

--- install/install.js
+++ install/install.js
@@ -3,20 +3,23 @@ const fs = require("fs");
 const chalk = require("chalk");
 
 function node() {
+	printStart("NVM");
+
 	let isInstalled = shell.exec("which -s nvm").code;
 
 	if (isInstalled === 0) {
-		print("NVMk");
 		shell.exec("curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash");
 		shell.exec("nvm install node");
 	}
 
-	// Install node packages
+	printStep("Installing Extensions");
 	// shell.exec("npm i -g weatherterm");
+
+	printComplete();
 }
 
 function tmux() {
-	print("Tmux");
+	printStart("Tmux");
 
 	shell.cd("~/");
 
@@ -28,10 +31,12 @@ function tmux() {
 	// Symlink
 	shell.touch(".tmux.conf");
 	shell.ln("-sf", "~/dotfiles/tmux.conf", ".tmux.conf");
+
+	printComplete();
 }
 
 function vscode() {
-	print("VSCode");
+	printStart("VSCode");
 
 	shell.cd("~/");
 
@@ -42,17 +47,32 @@ function vscode() {
 		shell.rm(destinationFile)
 	}
 
+	// Current Extensions
+	printStep("Listing Current Extensions");
+	shell.exec("code --list-extensions | xargs -L 1 echo code --install-extension");
+
 	// Symlink
 	shell.touch(destinationFile);
 	shell.ln("-sf", "~/dotfiles/vscode/settings.json", destinationFile);
 
 	// Extentions
+	printStep("Installing Extensions");
 	shell.exec("code --install-extension PeterJausovec.vscode-docker");
 	shell.exec("code --install-extension vscodevim.vim");
+
+	printComplete();
 }
 
-function print(stage) {
-	console.log(chalk.blue(stage));
+function printStart(stage) {
+	console.log(chalk.green(`\n${stage}`));
+}
+
+function printComplete(stage) {
+	console.log(chalk.white("-----------------------------------------------------"));
+}
+
+function printStep(step) {
+	console.log(chalk.yellow(`-> ${step}`));
 }
 
 node();
